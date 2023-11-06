0137d3c61dbe243b512c334d10ef87d1a8e75264
Jennifer Hajduk <jhajduk@microsoft.com>
2023-11-06 19:48:14 +0000

    create silk volumea

--- create-silkvolumes.ps1
+++ create-silkvolumes.ps1
@@ -1,30 +1,34 @@
-    
-$targetComputerNamesOrIPs = Get-Content -Path .\computerNamesOrIPs.txt
-
-$fileName = "file-" + (Get-Random) + ".txt"
-$fileLocation = ".\"
-
-New-Item -Path $fileLocation -Name $fileName
+#Log In
 $Credential = Get-Credential
 
-$devicePath = '/dev/sdc'
+#Get commands mapping
+$mappings = Get-Content -Path .\volumeMapping.json | ConvertFrom-Json
 
-#Get Computer Information
-foreach ($computerNameOrIP in $targetComputerNamesOrIPs)
-{
+$commands = $mappings.commands
 
-   $ssh = New-SSHSession -ComputerName $computerNameOrIP -Credential $Credential
-
-   #Change command to sdparm
-   $output = $(Invoke-SSHCommand -SSHSession $ssh -Command "sudo hdparm -I ${devicePath} | grep 'Logical/Physical Sector size'").Output
-
-   $computerNameOrIP = $computerNameOrIP.Trim()
-
-   $output = $output -split '\s+' | Select-Object -Last 2
-   $output = $output.Trim()
-
-   $structureData = "$computerNameOrIP,$output"
-   Add-Content -Path .\file*.txt -Value $structureData
-
+# SSH into the Linux machine and execute the commands
+try {
+    $session = New-SSHSession -ComputerName $linuxMachineIP -Credential (New-Object PSCredential -ArgumentList ($sshUsername, $sshPassword))
+}
+catch {
+    Write-Host "Could not open the session: $_"
 }
 
+if ($session) {
+    foreach ($command in $commands) {
+        try {
+            $result = Invoke-SSHCommand -SessionId $session.SessionId -Command $command -ErrorAction Stop
+            if ($result.ExitCode -ne 0) {
+                Write-Host "Command failed with exit code $($result.ExitCode): $command"
+                # Optionally, you can choose to break out of the loop or continue with the next command.
+            }
+        }
+        catch {
+            Write-Host "Error executing command: $_"
+            # Optionally, you can choose to break out of the loop or continue with the next command.
+        }
+    }
+    Remove-SSHSession -SessionId $session.SessionId
+} else {
+    Write-Host "Failed to establish an SSH session."
+}
---
+++ find-silkGUID.ps1
@@ -0,0 +1,56 @@
+﻿$targetComputerNamesOrIPs = Get-Content -Path .\computerNamesOrIPs.txt
+
+$fileName = "file-" + (Get-Random) + ".txt"
+$fileLocation = ".\"
+
+New-Item -Path $fileLocation -Name $fileName
+$Credential = Get-Credential
+
+$devicePath = '/dev/sdc'
+
+# Get Computer Information
+foreach ($computerNameOrIP in $targetComputerNamesOrIPs)
+{
+   $computerNameOrIP = $computerNameOrIP.Trim()
+   
+   try 
+   {
+        # Check if the computer is reachable before establishing an SSH session
+        $isReachable = Test-Connection -ComputerName $computerNameOrIP -Count 1 -ErrorAction Stop
+
+        if ($isReachable)
+        {
+            $ssh = New-SSHSession -ComputerName $computerNameOrIP -Credential $Credential
+        }
+        else
+        {
+            Write-Host "Computer $computerNameOrIP is not reachable."
+            continue  # Skip to the next computer
+        }
+   }
+   catch
+   {
+        Write-Host "Could not establish a connection to ${computerNameOrIP}: $_"
+        continue  # Skip to the next computer
+   }
+
+   try
+   {
+       # Change command to sdparm
+       $output = $(Invoke-SSHCommand -SSHSession $ssh -Command "sudo hdparm -I ${devicePath} | grep 'Logical/Physical Sector size'").Output
+
+       $output = $output -split '\s+' | Select-Object -Last 2
+       $output = $output.Trim()
+
+       $structureData = "$computerNameOrIP,$output"
+       Add-Content -Path ".\$fileName" -Value $structureData
+   }
+   catch
+   {
+       Write-Host "Error executing SSH command on ${computerNameOrIP}: $_"
+   }
+   finally
+   {
+       Remove-SSHSession -SessionId $ssh.SessionId
+   }
+}
