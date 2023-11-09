9a68482e24596d748b9c69cc0562cf9a5124ff3e
Jennifer Hajduk <jhajduk@microsoft.com>
2023-11-09 14:35:32 -0800

    adding shell script

--- volumeMapping.sh
+++ volumeMapping.sh
@@ -1,75 +1,66 @@
 #!/bin/bash
 
-# Function to handle errors and exit the script
-handle_error() {
-    echo "Error: $1"
+# Function to print error message and exit
+error_exit()
+{
+    echo "Error: $1" 1>&2
     exit 1
 }
 
-# Function to execute a command with error handling
-execute_command() {
-    local cmd="$1"
-    local description="$2"
-    
-    echo "Executing: $description"
-    
-    # Run the command and capture the exit code
-    if ! eval "$cmd"; then
-        handle_error "$description failed"
-    fi
-}
+# Create physical volumes
+pvcreate /dev/mapper/mpathb /dev/mapper/mpathc /dev/mapper/mpathd /dev/mapper/mpathe /dev/mapper/mpathf /dev/mapper/mpathg /dev/mapper/mpathh /dev/mapper/mpathi ||
+    error_exit "Failed to create initial physical volumes."
 
-# Perform the required actions with error handling
+# Create volume group
+vgcreate prdvg /dev/mapper/mpathb /dev/mapper/mpathc /dev/mapper/mpathd /dev/mapper/mpathe /dev/mapper/mpathf /dev/mapper/mpathg /dev/mapper/mpathh /dev/mapper/mpathi ||
+    error_exit "Failed to create volume group."
 
-# Create Physical Volumes (PVs)
-pvcreate_cmds=(
-    "/dev/mapper/mpathb"
-    "/dev/mapper/mpathc"
-    "/dev/mapper/mpathd"
-    "/dev/mapper/mpathe"
-    "/dev/mapper/mpathf"
-    "/dev/mapper/mpathg"
-    "/dev/mapper/mpathh"
-    "/dev/mapper/mpathi"
-)
+# Create logical volume
+lvcreate -L 80T -i 8 -I 4M -n epicprd01lv prdvg ||
+    error_exit "Failed to create logical volume."
 
-for pv_cmd in "${pvcreate_cmds[@]}"; do
-    execute_command "pvcreate $pv_cmd" "Creating Physical Volume: $pv_cmd"
-done
-
-# Create Volume Group (VG)
-execute_command "vgcreate prdvg /dev/mapper/mpathb /dev/mapper/mpathc /dev/mapper/mpathd /dev/mapper/mpathe /dev/mapper/mpathf /dev/mapper/mpathg /dev/mapper/mpathh /dev/mapper/mpathi" "Creating Volume Group: prdvg"
-
-# Create Logical Volume (LV)
-execute_command "lvcreate -L 80T -i 8 -I 4M -n epicprd01lv prdvg" "Creating Logical Volume: epicprd01lv"
-
-# Format the Logical Volume with XFS
-execute_command "mkfs.xfs /dev/mapper/prdvg-epicprd01lv" "Formatting Logical Volume as XFS"
+# Make XFS filesystem
+mkfs.xfs /dev/mapper/prdvg-epicprd01lv ||
+    error_exit "Failed to format the filesystem."
 
 # Create mount directory
-execute_command "mkdir -p /epic/prd01" "Creating Mount Directory: /epic/prd01"
+mkdir -p /epic/prd01 ||
+    error_exit "Failed to create mount directory."
 
-# Mount the Logical Volume
-execute_command "mount /dev/mapper/prdvg-epicprd01lv /epic/prd01" "Mounting Logical Volume to /epic/prd01"
+# Mount the filesystem
+mount /dev/mapper/prdvg-epicprd01lv /epic/prd01 ||
+    error_exit "Failed to mount the filesystem."
 
-# Create Physical Volumes for the second set
-pvcreate_cmds=(
-    "/dev/mapper/mpathaa1"
-    "/dev/mapper/mpathab1"
-    "/dev/mapper/mpathaf1"
-    "/dev/mapper/mpathae1"
-    "/dev/mapper/mpathag1"
-    "/dev/mapper/mpathah1"
-    "/dev/mapper/mpathac1"
-    "/dev/mapper/mpathad1"
-    "/dev/mapper/mpathai1"
-)
+# Create additional physical volumes
+pvcreate /dev/mapper/mpathaa1 /dev/mapper/mpathab1 /dev/mapper/mpathaf1 /dev/mapper/mpathae1 /dev/mapper/mpathag1 /dev/mapper/mpathah1 /dev/mapper/mpathac1 /dev/mapper/mpathad1 ||
+    error_exit "Failed to create additional physical volumes."
 
-for pv_cmd in "${pvcreate_cmds[@]}"; do
-    execute_command "pvcreate $pv_cmd" "Creating Physical Volume: $pv_cmd"
-done
+# Add entries to /etc/fstab
+echo "/dev/mapper/mpathai1 /epic/mdrp/jrn ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathai1."
 
-# Add your remaining commands here for mounting, etc.
+echo "/dev/mapper/mpathaa1 /epic/mdrp01 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathaa1."
 
-# Handle completion
-echo "All commands executed successfully."
+echo "/dev/mapper/mpathab1 /epic/mdrp02 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathab1."
+
+echo "/dev/mapper/mpathac1 /epic/mdrp03 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathac1."
+
+echo "/dev/mapper/mpathad1 /epic/mdrp04 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathad1."
+
+echo "/dev/mapper/mpathae1 /epic/mdrp05 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathae1."
+
+echo "/dev/mapper/mpathaf1 /epic/mdrp06 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathaf1."
+
+echo "/dev/mapper/mpathag1 /epic/mdrp07 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathag1."
+
+echo "/dev/mapper/mpathah1 /epic/mdrp08 ext4 _netdev 0 0" >> /etc/fstab ||
+    error_exit "Failed to add fstab entry for mpathah1."
+
+echo "All operations completed successfully."
